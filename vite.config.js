eb77254a8b0181e68839cfeb62aa3107cac2a05c
Mich personal <michpersonal@macbookair.home>
2025-04-04 22:50:48 -0400

    update vite.config.js to change manifest.json location

--- vite.config.js
+++ vite.config.js
@@ -4,26 +4,10 @@ import laravel from 'laravel-vite-plugin';
 export default defineConfig({
     plugins: [
         laravel({
-            input: [
-                'resources/css/app.css',
-                'resources/js/app.js',
-            ],
+            input: ['resources/css/app.css', 'resources/js/app.js'],
             refresh: true,
+            // Add this line to change the manifest path
+            buildDirectory: './build/',
         }),
     ],
-    build: {
-        manifest: true,
-        outDir: 'public/build',
-        emptyOutDir: true,
-        rollupOptions: {
-             input: {
-                app: 'resources/js/app.js',  // Specific input path for JS
-                css: 'resources/css/app.css',  // Specific input path for CSS
-            },
-            output: {
-                // Ensure output is correctly set to public/build
-                dir: 'public/build',
-            },
-        },
-    },
 });
